477073c857b3989615f85ae65315d9a4e4b63803
Lucian Tuduce <lucian.tuduce@fortech.ro>
2015-10-06 15:32:06 +0300

    Fixed minor bug

--- src/main/java/com/fortech/service/MarketRuleService.java
+++ src/main/java/com/fortech/service/MarketRuleService.java
@@ -2,12 +2,17 @@ package com.fortech.service;
 
 
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 @Stateless
 public class MarketRuleService {
 	
+	@PersistenceContext(unitName="car-application")
+	private EntityManager entityManager;
+	
 	public String printCeva(){
-		System.out.println("INJECTTTTTTT");
-		return "inject";
+		System.out.println("INJECT ceva dsdaawew");
+		return "INJECT ceva dsdaa";
 	}
 }
